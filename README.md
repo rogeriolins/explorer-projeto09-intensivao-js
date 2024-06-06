2c2ea2269d6c392bc718c947dc7cb1ebd8c56a75
Rogerio Lins <rogerio_lins2002@yahoo.com>
2024-06-06 10:43:48 -0300

    Project JS Finished

---
+++ README.md
@@ -0,0 +1,72 @@
+<h1 align="center"> Explorer - Intensivão Explorer Sem Limites</h1>
+
+<p align="center">
+Projeto para a formação Fullstack da Rocketseat para ensino de tecnologias WEB. <br/>
+<a href="https://www.rocketseat.com.br/formacao/fullstack?utm_source=github&utm_medium=explorer-projeto-intensivao-js-02&utm_campaign=capture-explorer&utm_term=organic&utm_content=descricao-github-rogeriolins">Faça essa formação em vídeo clicando aqui.</a>
+</p>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=49AA26&labelColor=000000">
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="Projeto Explorer" src=".github/project-02.jpg" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML e CSS
+- Git e Github
+- JavaScript
+
+## 💻 Projeto
+
+O Desafio de JS - Proposto 02 - tem as seguintes finalidades:
+
+Você pode apresentar o resultado tanto no **console** quanto em um **alerta** no navegador.
+
+1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+
+2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+
+3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+
+5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+
+6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+
+7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+
+8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+
+9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+
+10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+
+- [Acesse o projeto finalizado, online](https://rogeriolins.github.io/explorer-desafio-js-proposto-01)
+
+- [Assistir aulas](https://www.rocketseat.com.br/formacao/fullstack?utm_source=github&utm_medium=explorer-projeto-intensivao-js-02&utm_campaign=capture-explorer&utm_term=organic&utm_content=descricao-github-rogeriolins)
+
+## 🔖 Layout
+
+Você pode visualizar o projeto ao se matricular na formação.
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT.
+
+---
+
+Feito com ♥ by Rocketseat e codado por [Rogerio Lins](https://instagram.com/rogeriolins.dev) :wave: [Participe da nossa comunidade!](https://discord.gg/rocketseat)
--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // 1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
 const myHello = `Exercicio 1: \n\nHello World!`
 console.log(myHello)
-alert(myHello)
+// alert(myHello)
 
 // 2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
 let numberOne = 25
@@ -12,24 +12,89 @@ myResult = `Exercicio 2: \n\nA soma do número ${numberOne} + ${numberTwo} = ${(
   numberOne + numberTwo
 ).toFixed(2)}`
 console.log(myResult)
-alert(myResult)
+// alert(myResult)
 
 // 3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
 let numberRandom = 23.5
 myResult2 = `Exercicio 3: \n\n`
-console.log(typeof numberRandom)
-if (typeof numberRandom == Number) {
+
+if (typeof numberRandom == "number") {
   myResult2 += `É um número`
 } else {
   myResult2 += `Não é um número`
 }
-
-//     💡 Para saber o tipo de dado você pode usar o operador `typeof`
+console.log(myResult2)
+// alert(myResult2)
 
 // 4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+let strString = "JavaScript is The king"
+myResult3 = `Exercicio 4: \n\n`
+
+if (typeof strString == "string") {
+  myResult3 += `É uma string`
+} else {
+  myResult3 += `Não é uma string`
+}
+console.log(myResult3)
+// alert(myResult3)
+
 // 5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+let myBoolean = false
+myResult4 = `Exercicio 5: \n\n`
+
+if (typeof myBoolean == "boolean") {
+  myResult4 += `É um Booleano`
+} else {
+  myResult4 += `Não é um Booleano`
+}
+console.log(myResult4)
+// alert(myResult4)
+
 // 6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+let numFirst = 10
+let numSecond = 22
+myResult5 = `Exercicio 6: \n\nA subtração do número ${numFirst} - ${numSecond} = ${(
+  numFirst - numSecond
+).toFixed(2)}`
+console.log(myResult5)
+// alert(myResult5)
+
 // 7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+let numFirst1 = 11
+let numSecond1 = 33
+myResult6 = `Exercicio 7: \n\nA multiplicação do número ${numFirst1} * ${numSecond1} = ${(
+  numFirst1 * numSecond1
+).toFixed(2)}`
+console.log(myResult6)
+// alert(myResult6)
+
 // 8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+let numFirst2 = 400
+let numSecond2 = 40
+myResult7 = `Exercicio 8: \n\nA divisão do número ${numFirst2} / ${numSecond2} = ${(
+  numFirst2 / numSecond2
+).toFixed(2)}`
+console.log(myResult7)
+// alert(myResult7)
+
 // 9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+let numberPar = 13
+myResult8 = `Exercicio 9: \n\n`
+if (numberPar % 2 == 0) {
+  myResult8 += `O número ${numberPar} é um numero PAR`
+} else {
+  myResult8 += `O número ${numberPar} é um numero IMPAR`
+}
+console.log(myResult8)
+// alert(myResult8)
+
 // 10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+let numberImpar = 12
+myResult9 = `Exercicio 10: \n\n`
+if (numberImpar % 2 == 1) {
+  myResult9 += `O número ${numberImpar} é um numero IMPAR`
+} else {
+  myResult9 += `O número ${numberImpar} é um numero PAR`
+}
+console.log(myResult9)
+// alert(myResult9)
